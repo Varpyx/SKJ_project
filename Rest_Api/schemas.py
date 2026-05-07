e4436ddce2ff3247b0c9a974b8124b15b28a0e07
Varpyx <prihlasmetam@seznam.cz>
2026-05-07 15:46:45 +0200

    ukoly 1.3 a 1.4

--- Rest_Api/schemas.py
+++ Rest_Api/schemas.py
@@ -65,8 +65,8 @@ class FileUploadResponse(BaseModel):
     # gt=0 (greater than) – velikost musí být kladné číslo, prázdný soubor nedává smysl
     size: int = Field(..., description="Velikost souboru v bytech", gt=0, le=10_485_760)
 
-    # ... znamená povinné pole – cesta musí být vždy uložena pro pozdější přístup
-    path: str = Field(..., description="Cesta k souboru na disku")
+    # SMAZANO... znamená povinné pole – cesta musí být vždy uložena pro pozdější přístup
+    #path: str = Field(..., description="Cesta k souboru na disku")
 
     # from_attributes=True umožňuje vytvořit schéma přímo z SQLAlchemy objektu:
     # FileUploadResponse.model_validate(db_file_object)
@@ -104,8 +104,8 @@ class FileMetadata(BaseModel):
         pattern=r"^[\w\-. ]+$"
     )
 
-    # ... znamená povinné pole – cesta nutná pro stažení souboru
-    path: str = Field(..., description="Cesta k souboru na disku")
+    #SMAZANO ... znamená povinné pole – cesta nutná pro stažení souboru
+    #path: str = Field(..., description="Cesta k souboru na disku")
 
     # gt=0 (greater than) – velikost musí být kladné číslo, prázdný soubor nedává smysl
     # Přidáno 'le' pro maximální velikost (10 MB)
